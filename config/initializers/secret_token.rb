170f2893bdc966d207cf8cdfe60d2bd866d9f454
vladimirb <pro.barsukov@gmail.com>
2013-10-29 00:36:07 +0400

    first initialize commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SponsorpayTest::Application.config.secret_token = 'd0e6b11e6d893e893c958468164ff481f1a6ab8f94e3e10042ab17d4f85412da827d21d0583085dbba63d38b107d9dd7919690828bc557afe5c963254eb4789f'
